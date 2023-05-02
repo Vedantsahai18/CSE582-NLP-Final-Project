f3ffc654e4688d1a2ce39fee93ddf857f19a61d0
Vedant Sahai <vedantsahai18@gmail.com>
2023-05-02 00:34:50 -0400

    ensemble code updated

--- ensemble/ensemble_generate.py
+++ ensemble/ensemble_generate.py
@@ -1,55 +1,73 @@
 from transformers import AutoTokenizer
 import os
 import pandas as pd
-import numpy as np
 from ensemble_test import evaluate
 from transformers import AutoModelForSeq2SeqLM
-import numpy as np
+from memsum.summarizers import MemSum
+from tqdm import tqdm
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
-def abstractive(path,text):
-    # generate a summary for the test dataset
-    tokenizer = AutoTokenizer.from_pretrained(path,local_files_only=True, cache_dir="model/")
-    inputs = tokenizer(text, return_tensors="pt").input_ids
-    model = AutoModelForSeq2SeqLM.from_pretrained(path)
-    outputs = model.generate(inputs, max_new_tokens=15, do_sample=False)
-    final_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
-    return final_text
+def abstractive(pathm,text):
+  # generate a summary for the test dataset
+  tokenizer = AutoTokenizer.from_pretrained(pathm,local_files_only=True, cache_dir="model/")
+  inputs = tokenizer(text, return_tensors="pt").input_ids
+  model = AutoModelForSeq2SeqLM.from_pretrained(pathm)
+  outputs = model.generate(inputs, max_new_tokens=20, do_sample=False)
+  final_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
+  return final_text
 
-def extractive(path,text):
-    
-    ext_text = None
-    return ext_text
+def extractive(pathm,corpus):
+  model = MemSum( pathm, "vocabulary_200dim.pkl", gpu = 0 ,  max_doc_len = 500  ) 
+  for data in tqdm(corpus):
+    extracted_summary = model.extract( [data["text"]], p_stop_thres = 0.6, max_extracted_sentences_per_document = 7 )[0]
+    ext_text = "\n".join(extracted_summary)
+  return ext_text
 
 
 def main():
   #loading the dataset
   df = pd.read_csv('corpusfinal.csv')
+  df_sample = df.sample(frac=0.5, replace=True, random_state=1)
 
-  #loading the model
-  model_checkpoint = "t5-small"
+  # #loading the model
+  path_abstractive = "t5-small"
+  path_extractive = "model_batch_3360.pt"
 
-  # generate a summary for the test dataset
+  # # generate a summary for the test dataset
   df_test = pd.DataFrame(columns=['gold_summary','modelsummary'])
 
-  for index,row in df.iterrows():
+  for index,row in df_sample.iterrows():
+
     text = row["document"]
     summary = row["abs_summary"]
+
     print("Original Text : ")
     print(text)
+    print()
+
+    # getting the extractive summary
     print("Extractive Summary : ")
-    ext_text = extractive("model/",text)
+    corpus_lst = []
+    corpus_dict = {}
+    corpus_dict['text'] = text.split(".")
+    corpus_lst.append(corpus_dict)
+    ext_text = extractive(path_extractive,corpus_lst)
     print(ext_text)
+    print()
+
+  #  # getting the abstractive summary
     print("Abstractive Summary : ")
-    final_text = abstractive("model/",ext_text)
+    final_text = abstractive(path_abstractive,ext_text)
     print(final_text)
+
     print(25*"=")
     df_test = df_test.append({'gold_summary':summary,'modelsummary':final_text},ignore_index=True)
 
-  # scores = evaluate( df_test, 0.6, 7)
-  # print("ROGUE SCORES")
-  # print(scores)
+  scores = evaluate( df_test, 0.6, 7)
+  print("ENSEMBLE ROGUE SCORES")
+  print(scores)
+
 
 if __name__ == "__main__":
     main()
--- ensemble/ensemble_test.py
+++ ensemble/ensemble_test.py
@@ -1,5 +1,4 @@
 
-from summarizers import MemSum
 from rouge_score import rouge_scorer
 import numpy as np
 import pandas as pd
@@ -9,18 +8,16 @@ def evaluate( corpus, p_stop, max_extracted_sentences ):
     rouge_cal = rouge_scorer.RougeScorer(['rouge1','rouge2', 'rougeLsum'], use_stemmer=False)
     for index,rows in corpus.iterrows():
         gold_summary = rows["abs_summary"]
-        extracted_summary = rows["modelsummary"]
-        # extracted_summary = model.extract( [data["text"]], p_stop_thres = p_stop, max_extracted_sentences_per_document = max_extracted_sentences )[0]
-        # score = rouge_cal.score( "\n".join( gold_summary ), "\n".join(extracted_summary)  )
+        extracted_summary = rows["ext_summary"]
         score = rouge_cal.score( gold_summary, extracted_summary  )
         scores.append( [score["rouge1"].fmeasure, score["rouge2"].fmeasure, score["rougeLsum"].fmeasure ] )
     
     return np.asarray(scores).mean(axis = 0)
 
 def main():
-    df = pd.read_csv('data/corpusfinal.csv')
-    df_test = df.iloc[:int(len(df)*0.3)]
-    evaluate( df_test, 0.6, 7)
+    df = pd.read_csv('../dataset/corpusfinal.csv')
+    df_test = df.iloc[:int(len(df)*0.1)]
+    print(evaluate( df_test, 0.6, 7))
 
 if __name__ == "__main__":
     main()
